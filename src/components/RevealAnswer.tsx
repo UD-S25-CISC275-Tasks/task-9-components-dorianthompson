7867d47521cf07cb0f2b2a45090afb9153e1eb91
Dorian <thompsondorian2@gmail.com>
2025-03-05 13:55:55 -0500

    Passed Reveal Answer test cases

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,14 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): React.JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [isVisible, setIsVisible] = useState<boolean>(false);
+    const handleClick = () => {
+        setIsVisible(!isVisible);
+    };
+    return (
+        <div>
+            <Button onClick={handleClick}>Reveal Answer</Button>
+            {isVisible && 42}
+        </div>
+    );
 }
